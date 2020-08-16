b349b694ee2df6ff76c658c9140cc15733efb690
jwe4 <jeweaver@gmail.com>
2020-08-15 20:45:09 -0700

    finished exercise

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,13 +1,5 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyDei2V7hyxHTjvv9QxPDwH_1gP9zUzhdFQ';
-
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
-  params: {
-    part: 'snippet',
-    type: 'video',
-    maxResults: 5,
-    key: KEY,
-  },
 });
